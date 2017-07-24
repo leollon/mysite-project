5afdf57d37309a89c49468fd3fca43eefea9d124
4Cooper <xcutewjz87@gmail.com>
2017-07-24 23:11:56 +0800

    Delete field article_summary

--- article/admin.py
+++ article/admin.py
@@ -6,7 +6,7 @@ class ArticleAdmin(admin.ModelAdmin):
     """
         Article model should be editable in thd admin interface.
     """
-    list_display = ('title', 'article_summary', 'article_body')
+    list_display = ('title', 'article_body')
     list_filter = ('created_time', 'view_times')
     ordering = ('created_time', 'view_times')
     readonly_fields = ('view_times',)
