0960cc9b314158c4375aa7c112f9af3708e7ef74
4Cooper <xcutewjz87@gmail.com>
2017-07-24 23:54:58 +0800

    change posts --> post

--- article/urls.py
+++ article/urls.py
@@ -5,7 +5,7 @@ app_name = 'article'
 
 urlpatterns = [
     url(r'^$', views.ArticleList.as_view(), name='index'),
-    url(r'^posts/(?P<pk>[\d]+)/$', views.ArticleDetail.as_view(), name='detail'),
+    url(r'^post/(?P<pk>[\d]+)/$', views.ArticleDetail.as_view(), name='detail'),
     url(r'^write/$',  views.ArticleCreate.as_view(), name='write'),
     url(r'^edit/(?P<pk>[\d]+)/$', views.ArticleUpdate.as_view(), name='edit'),
 ]
