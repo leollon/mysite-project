c1061af4bbdc8c3dedbec3820adf779b3a881765
4Cooper <xcutewjz87@gmail.com>
2017-07-24 23:10:55 +0800

    add ArticleCategory

--- article/models.py
+++ article/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.shortcuts import reverse
+from article_category.models import ArticleCategory
 
 
 class Article(models.Model):
@@ -7,10 +8,10 @@ class Article(models.Model):
         an article model
     """
     title = models.CharField(max_length=256)
-    article_summary = models.TextField()
     article_body = models.TextField()
     created_time = models.DateField(auto_now=True)
     view_times = models.IntegerField(default=0)
+    category = models.ForeignKey(ArticleCategory, null=True)
 
     def __str__(self):
         return "%s" % self.title
