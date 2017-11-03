0bbf850952d95070bf5fc64a18eb508d5794696e
leollon <ccsapp.tux@gmail.com>
2017-11-03 11:15:36 +0800

    pagination, add form_class to customize article's editor, article_body markdown style

--- article/views.py
+++ article/views.py
@@ -1,53 +1,78 @@
 from django.views.generic import DetailView
-from article.models import Article
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView
+from article.forms import CreateArticleForm, EditAriticleForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.conf import settings
+
+import mistune
+from article.utils import HightlightRenderer
+from article.models import Article
+
+per_page = getattr(settings, 'PER_PAGE')
 
 
-class ArticleDetail(DetailView):
+class ArticleList(ListView):
+    """Index homepage
     """
-    the detail of each article
+    template_name = "index.html"
+    model = Article
+    context_object_name = 'articles_list'
+
+    def get_context_data(self, **kwargs):
+        context = super(ArticleList, self).get_context_data(**kwargs)
+        articles_list = Article.objects.order_by('-created_time').all()
+        renderer = HightlightRenderer()
+        markdown = mistune.Markdown(escape=True, hard_wrap=True,
+                                    renderer=renderer)
+        # paginator paginate an ordered list
+        paginator = Paginator(articles_list, per_page=per_page)
+        page = self.request.GET.get('page')
+        try:
+            articles = paginator.page(page)
+            for article in articles:
+                article.article_body = markdown(article.article_body)
+        except PageNotAnInteger:
+            articles = paginator.page(1)
+        except EmptyPage:
+            articles = paginator.page(paginator.num_pages)
+        context['articles'] = articles
+        return context
+
+
+class CreateArticleView(CreateView):
+    """Class-based view function used to write an article
+    """
+    template_name = 'article_write.html'
+    form_class = CreateArticleForm
+
+
+class UpdateArticleView(UpdateView):
+    """View function for editing a posted article
+    """
+    template_name = 'article_write.html'
+    model = Article
+    context_object_name = 'article'
+    form_class = EditAriticleForm
+    # fields = ['title', 'article_body']
+
+    def get_object(self):
+        article = super(UpdateArticleView, self).get_object()
+        return article
+
+
+class ArticleDetailView(DetailView):
+    """The detail of each article
     """
     model = Article
     template_name = 'article_detail.html'
     context_object_name = 'article'
 
     def get_object(self):
-        article = super(ArticleDetail, self).get_object()
+        renderer = HightlightRenderer()
+        markdown = mistune.Markdown(escape=True, hard_wrap=True, renderer=renderer)
+        article = super(ArticleDetailView, self).get_object()
         article.view_times += 1
         article.save()
+        article.article_body = markdown(article.article_body)
         return article
-
-
-class ArticleList(ListView):
-    """
-    index homepage
-    """
-    template_name = "index.html"
-    context_object_name = 'article_list'
-    model = Article
-
-
-class ArticleCreate(CreateView):
-    """
-    class-based view function used to write an article
-    """
-    template_name = 'article_write.html'
-    model = Article
-    fields = ['title', 'article_body']
-
-
-class ArticleUpdate(UpdateView):
-    """
-    view function for editing a posted article
-    """
-    template_name = 'article_edit.html'
-    model = Article
-    context_object_name = 'article'
-    fields = ['title', 'article_summary', 'article_body']
-
-    def get_object(self):
-        article = super(ArticleUpdate, self).get_object()
-        return article
-
-
