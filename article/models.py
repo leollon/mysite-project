2f48037a2469dce719d8f314a5dc20fd5d0af2a3
leollon <xcutewjz87@gmail.com>
2017-11-03 11:09:20 +0800

    add python_2_unicode_compatible and view_times use PositiveIntegerField

--- article/models.py
+++ article/models.py
@@ -1,22 +1,21 @@
 from django.db import models
 from django.shortcuts import reverse
+from django.utils.encoding import python_2_unicode_compatible
 from article_category.models import ArticleCategory
 
 
+@python_2_unicode_compatible
 class Article(models.Model):
     """
-        an article model
+    an article model - control the way to access data in the database
     """
     title = models.CharField(max_length=256)
     article_body = models.TextField()
-    created_time = models.DateField(auto_now=True)
-    view_times = models.IntegerField(default=0)
+    created_time = models.DateField(auto_now_add=True)
+    view_times = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ArticleCategory, null=True)
 
     def __str__(self):
-        return "%s" % self.title
-
-    def __unicode__(self):
         return "%s" % self.title
 
     @staticmethod
