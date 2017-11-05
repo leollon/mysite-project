4427fbd82dc0c332d5fc29122b1847c0e3772003
leollon <xcutewjz87@gmail.com>
2017-11-06 00:26:55 +0800

    hange password encrypt hasher

--- mysite/settings.py
+++ mysite/settings.py
@@ -102,6 +102,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Password Encrypt Hasher
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.SHA1PasswordHasher',
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+    'django.contrib.auth.hashers.CryptPasswordHasher',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
