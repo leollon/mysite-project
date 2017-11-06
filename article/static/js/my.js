a243faef0284b3c45d6186883c0f69add478b2c8
leollon <xcutewjz87@gmail.com>
2017-11-06 12:46:11 +0800

    markdown editor and preview it right way

--- article/static/js/my.js
+++ article/static/js/my.js
@@ -1,2 +1,35 @@
-head = document.getElementById('blog-head');
-head.innerHTML = document.getElementById('article-title').innerHTML;
+var splited_url = window.location.href.split('/');
+var is_post_detail_page = splited_url.indexOf('post');
+var is_editor_page = document.getElementById('article-editor');
+
+
+if (is_post_detail_page && !is_editor_page) {
+    var head = document.getElementById('blog-head');
+    head.innerHTML = document.getElementById('article-title').innerHTML;
+//    console.log(is_editor_page);  // for test
+}
+
+function markdownToHtmlHandler(text) {
+    'use strict';
+    var converter = new showdown.Converter();
+    converter.setFlavor('github');
+    return converter.makeHtml(text);
+}
+
+// 这里后期可能要做监听事件的监听策略了，监听了不必要的事件了，比如监听单选框的输入了
+window.addEventListener('input', function editorPage() {
+    'use strict';
+//    console.log('hello, showdown'); // for test user input
+    if (is_editor_page) {
+
+        // editor
+        var id_title = document.getElementById('id_title');
+        var id_article_body = document.getElementById('id_article_body');
+
+        //  preview
+        var article_title_preview = document.getElementById('article-title');
+        var article_body_preview = document.getElementById('article-preview-body');
+        article_title_preview.innerHTML = id_title.value;
+        article_body_preview.innerHTML  = markdownToHtmlHandler(id_article_body.value);
+    }
+})
