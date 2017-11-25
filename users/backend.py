ad40968a9c60779e639984e2d9ea34272f135897
leollon <ccsapp.tux@gmail.com>
2017-11-25 22:05:51 +0800

    customize backend authenticate with email and password

---
+++ users/backend.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+customizing my own authentication system
+"""
+
+from users.models import User
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
+
+
+class EmailBackend(ModelBackend):
+
+    def authenticate(self, request, email=None, password=None, **credentials):
+        UserModel = get_user_model()
+        try:
+            user = UserModel.objects.get(email=email)
+        except User.DoesNotExist:
+            return None
+        else:
+            if user.check_password(password):
+                return user
