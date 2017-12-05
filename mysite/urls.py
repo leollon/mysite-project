8f023469392f07c7c38436241175714300498dd1
leollon <ccsapp.tux@gmail.com>
2017-12-05 18:27:29 +0800

    <fix> fix the wrong urls of category

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('article.urls')),
-    url(r'category', include('article_category.urls')),
+    url(r'category/', include('article_category.urls')),
     url('', include('users.urls')),
 ]
