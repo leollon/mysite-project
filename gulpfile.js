8f433e6e7e1c0368ca307d84b0bdb4a79ebe1ade
leollon <ccsapp.tux@gmail.com>
2018-10-10 12:06:03 +0800

    eslint --init

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,34 @@
-var gulp = require('gulp');
-var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
-var cleanCSS = require('gulp-clean-css');
-var imageMinify = require('gulp-imagemin');
-var del = require('del');
+var gulp = require("gulp");
+var rename = require("gulp-rename");
+var uglify = require("gulp-uglify");
+var cleanCSS = require("gulp-clean-css");
+var imageMinify = require("gulp-imagemin");
+var del = require("del");
 
 var paths = {
     css: {
-        orig: 'static/css/*.css',
-        dest: 'assets/css/'
+        orig: "static/css/*.css",
+        dest: "assets/css/"
     },
     js: {
-        orig: 'static/js/*.js',
-        dest: 'assets/js/'
+        orig: "static/js/*.js",
+        dest: "assets/js/"
     },
     img: {
-        orig: 'static/images/*.*',
-        dest: 'assets/images/'
+        orig: "static/images/*.*",
+        dest: "assets/images/"
     }
-}
+};
 
 function clean() {
-    return del(['assets/**', 'dist'])
+    return del(["assets/**", "dist"]);
 }
 
 function jsMinify() {
     return gulp.src(paths.js.orig) // 1. js 文件目录
         .pipe(uglify()) // 2. 压缩文件
         .pipe(rename({
-            extname: '.min.js'
+            extname: ".min.js"
         })) // 3. 重命名文件
         .pipe(gulp.dest(paths.js.dest)); // 4. 另存压缩后的文件
 }
@@ -37,13 +37,13 @@ function cssMinify() {
     // 压缩css文件
     return gulp.src(paths.css.orig)
         .pipe(cleanCSS({
-            format: 'beautify',
+            format: "beautify",
             level: 2
         }))
         .pipe(rename({
-            extname: '.min.css'
+            extname: ".min.css"
         }))
-        .pipe(gulp.dest(paths.css.dest))
+        .pipe(gulp.dest(paths.css.dest));
 }
 
 function imgMinifiy() {
@@ -56,8 +56,8 @@ function imgMinifiy() {
 }
 
 function auto() {
-    gulp.watch('static/css/*.css', cssMinify);
-    gulp.watch('static/js/*.js', jsMinify);
+    gulp.watch("static/css/*.css", cssMinify);
+    gulp.watch("static/js/*.js", jsMinify);
 }
 
 exports.compress = gulp.series(clean, gulp.parallel(cssMinify, jsMinify, imgMinifiy));
