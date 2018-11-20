c2abd9aa465750737783e5ab5aac272de0f97a6a
leollon <ccsapp.tux@gmail.com>
2018-11-20 17:04:17 +0800

    <add>article:Import article content from markdown file in coroutine

---
+++ utils/primer.py
@@ -0,0 +1,13 @@
+"""Prime a generator
+"""
+from functools import wraps
+
+
+def primer_generator(func):
+    """Decorator: primes `func` by advancing first `yield`"""
+    @wraps(func)
+    def primer(*args, **kwargs):
+        generator = func(*args, **kwargs) # get an generator object
+        next(generator) # prime the generator
+        return generator
+    return primer
