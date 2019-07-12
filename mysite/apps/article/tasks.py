bcff3f1fbc329c3d086781799f6fa0c41ea3c011
leollon <ccsapp.tux@gmail.com>
2019-07-12 20:52:25 +0800

    <remove>:task:remove unused imported cache

--- mysite/apps/article/tasks.py
+++ mysite/apps/article/tasks.py
@@ -3,10 +3,10 @@ from django.db.models import F
 from .models import Article
 
 from celery import shared_task
-from utils.cache import cache
 
 
 @shared_task
 def increment_view_times(article_id):
     return Article.objects.filter(id=article_id).update(
-                view_times=F('view_times') + 1)
+        view_times=F("view_times") + 1
+    )
